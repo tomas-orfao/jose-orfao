d4a990f252bb3583619d328b0633ea0a71f2928e
tomas-orfao <orfaotomas@gmail.com>
2025-12-02 03:08:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,28 @@ document.addEventListener("DOMContentLoaded", function() {
         lastScroll = currentScroll;
     });
 });
+
+document.querySelectorAll(".pergunta").forEach(pergunta => {
+    pergunta.addEventListener("click", () => { //adicionar a opção de clicar em cada pergunta
+
+        const resposta = pergunta.nextElementSibling;   //a constante resposta é = à div que está abaixo da pergunta
+        const arrow = pergunta.querySelector(".arrow"); //a constante arrow é = à div arrow no html
+
+        resposta.classList.toggle("open"); //adiciona o open ou remove o open da class no css 
+        arrow.classList.toggle("rotate");
+
+        // Fechar as outras
+        document.querySelectorAll(".resposta").forEach(a => {
+            if (a !== resposta) a.classList.remove("open");
+        });
+        /* seleciona todas as .resposta do html document.querySelectorAll(".resposta")
+        vê cada uma forEach(a => { - neste caso "a" é só o nome que dás à variavel da resposta
+        e (a for diferente da resposta que vem imediatamente de uma pergunta que não selecionou) então 
+        a.classList.remove("open"); fecha a resposta */
+        
+        document.querySelectorAll(".arrow").forEach(seta => {
+            if (seta !== arrow) seta.classList.remove("rotate");
+        });
+
+    });
+});
