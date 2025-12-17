0abf3b674fe8f510ad361234adf1edbfab422b78
tomas-orfao <orfaotomas@gmail.com>
2025-12-17 23:28:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -100,3 +100,21 @@ window.addEventListener('scroll', () => {
     lastScroll = currentScroll;
 });
 
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target); // animate once
+            }
+        });
+    },
+    {
+        threshold: 0.2 // triggers when 20% is visible
+    }
+);
+
+fadeElements.forEach(el => observer.observe(el));
+
